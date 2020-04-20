baffda4a76e02670f8ea6f6c34f6d01e9344e728
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2020-04-20 21:50:09 +0200

    sound fx

---
+++ Masters project/Assets/GoblinSoundManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinSoundManager : MonoBehaviour
+{
+    public new AudioSource audio;
+    public List<AudioClip> clips = new List<AudioClip>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StepEvent()
+    { 
+        audio.pitch = Random.Range(0.9f, 1.1f);
+        int rand = Random.Range(0, clips.Count);
+        audio.PlayOneShot(clips[rand]);
+    }
+}
