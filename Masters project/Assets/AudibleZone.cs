4901009783cbdae2ebd10cfbe69ce835746f4808
JonasHalver <44400498+JonasHalver@users.noreply.github.com>
2020-07-16 17:39:20 +0200

    Audio fix
    can no longer hear everyone everywhere

---
+++ Masters project/Assets/AudibleZone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudibleZone : MonoBehaviour
+{
+    public AudioSource sound;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = CharacterController.theGoblin.transform;
+        sound = transform.parent.GetComponent<AudioSource>();
+        sound.Pause();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform == player)
+        {
+            sound.UnPause();
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform == player)
+        {
+            sound.Pause();
+        }
+    }
+}
